05c5c1f807c8729c3f3ce5d809e1db4d79bf73a2
The Podz <stefanpodz@yahoo.com>
2021-06-06 18:51:44 -0400

    added more employees to api call

--- src/utils/API.js
+++ src/utils/API.js
@@ -3,6 +3,6 @@ import axios from "axios";
 
 export default {
     getEmployee: function() {
-        return axios.get("https://randomuser.me/api/?results=20&nat=us");
+        return axios.get("https://randomuser.me/api/?results=30&nat=us");
     }
 }
