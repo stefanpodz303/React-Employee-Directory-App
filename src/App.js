802f86a5c078ef78f8992062f515c0f08a8ccd6d
The Podz <stefanpodz@yahoo.com>
2021-06-06 18:26:30 -0400

    added NavBar to App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 
 import './App.css';
+import NavBar from "./components/NavBar";
 import EmployeeContainer from "./components/EmployeeContainer";
 
 const App = () => {
   return (
     <>
-      
+      <NavBar />
       <EmployeeContainer />
     </>
   );
